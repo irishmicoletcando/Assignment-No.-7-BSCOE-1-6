89b0ebe0088a8ec052856889a7f5aea467318c5d
Irish Micole Cando <irishmicolecando@gmail.com>
2021-11-28 20:26:51 +0800

    Defined a function to count the words, vowels, and consonants and added input for the user

---
+++ Vowels_and_Consonants.py
@@ -0,0 +1,21 @@
+# Program 1:
+# Create a program that ask for a sentence as input.
+# Display the number of words, vowels and consonants in the input 
+# Ex.
+# Input: *depends on the user*
+# Output:
+# Words: 3
+# Vowels: 6
+# Consonants: 8
+
+def words_vowels_consonants():
+    user_words = input("Enter a word/words: ").lower()
+
+
+
+
+
+
+
+words_vowels_consonants()
+
