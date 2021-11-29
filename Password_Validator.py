ad1d5e43f96ad2323f4022026b2795a33b98351a
Irish Micole Cando <irishmicolecando@gmail.com>
2021-11-29 10:29:03 +0800

    Defined a function to validate the password

---
+++ Password_Validator.py
@@ -0,0 +1,23 @@
+# Program 2: Password validator
+# Create a program that check if password is valid
+# The password is valid if all criteria are met:
+# a. Greater than 15 letters
+# b. Have at least one capital letter
+# c. Have at least one number
+# d. Have at least one special char (!@#$%^&*()_+ etc)
+# Ex.
+# Input: P@ssw0rd+P@ssw0rd
+# Output: Valid
+
+# Tip: loop through each character of the input then process it letter by letter
+
+def password_validate():
+
+
+
+
+
+
+
+
+password_validate()
